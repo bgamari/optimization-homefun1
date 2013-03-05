bbeb2b98cd59618323cf3be48d5761c7b322b3b1
Ben Gamari <bgamari.foss@gmail.com>
2013-03-05 13:46:47 -0500

    q5: whitespace-cleanup

--- q5.hs
+++ q5.hs
@@ -14,7 +14,7 @@ import Linear
 -- in direction 'p' from point 'x' for function 'f' with gradient 'df'
 type LineSearch f a = (f a -> a) -> (f a -> f a) -> f a -> f a -> a
 
--- | Armijo condition    
+-- | Armijo condition
 armijo :: (Num a, Additive f, Ord a, Metric f)
        => a -> (f a -> a) -> (f a -> f a) -> f a -> f a -> a -> Bool
 armijo c1 f df x p a =
@@ -51,7 +51,7 @@ gradientDescent search f df x0 = iterate go x0
   where go x = let p = negated (df x)
                    a = search f df p x
                in x ^+^ a *^ p
-                
+
 -- | A beta expression 'beta df0 df1 p' is an expression for the
 -- conjugate direction contribution given the derivative 'df0' and
 -- direction 'p' for iteration 'k', 'df1' for iteration 'k+1'
@@ -94,7 +94,7 @@ bicInv' :: (Functor m, Distributive m, Additive m,
 bicInv' a0 a = iterate go a0
   where go ak = 2 *!! ak !-! ak !*! a !*! ak
 
--- | Inverse by iterative method of Ben-Israel and Cohen                
+-- | Inverse by iterative method of Ben-Israel and Cohen
 -- starting from 'alpha A^T'. Alpha should be set such that
 -- 0 < alpha < 2/sigma^2 where sigma denotes the largest singular
 -- value of A
@@ -114,7 +114,7 @@ newton :: (Num a, Ord a, Additive f, Metric f)
        => (f a -> a) -> (f a -> f a) -> (f a -> f (f a)) -> f a -> [f a]
 newton f df ddfInv x0 = iterate go x0
   where go x = x ^-^ ddfInv x !* df x
-       
+
 -- | Convert a nested-functor matrix to an hmatrix Matrix
 toHMatrix :: (Functor m, Foldable m, Foldable n, LA.Element a)
           => m (n a) -> LA.Matrix a
