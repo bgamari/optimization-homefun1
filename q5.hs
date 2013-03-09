92fbef56931a72b3b666b8866d4c4f92e58baaa3
Ben Gamari <bgamari.foss@gmail.com>
2013-03-09 11:42:08 -0500

    q5: Add Nesterov 1983

--- q5.hs
+++ q5.hs
@@ -142,6 +142,28 @@ fista l f df x0 = go x0 x0 1
                           y2 = x1 ^+^ (t1-1) / t2 *^ (x1 ^-^ x0)
                           in x1 : go x1 y2 t2
 
+-- | Nesterov 1983
+nesterov :: (Additive f, Functor f, Ord a, Floating a, Fractional a)
+         => a -> a -> (f a -> a) -> (f a -> f a) -> f a -> a -> [f a]
+nesterov kappa l f df x0 a0 = go x0 x0 a0
+  where go x0 y0 a0 = let x1 = y0 ^-^ df y0 ^/ l
+                          alphas = quadratic 1 (a0^2 - 1/kappa) (-a0^2)
+                          a1 = case filter (\x->x >= 0 && x <= 1) alphas of
+                                 a:_  -> a
+                                 []   -> error "No solution for alpha_{k+1}"
+                          b1 = a0 * (1 - a0) / (a0^2 + a1)
+                          y1 = x1 ^+^ b1 *^ (x1 ^-^ x0)
+                      in x1 : go x1 y1 a1
+
+-- | 'quadratic a b c' is the real solutions to a quadratic equation
+-- 'a x^2 + b x + c == 0'
+quadratic :: (Ord a, Floating a, Fractional a) => a -> a -> a -> [a]
+quadratic a b c
+    | discr < 0 = []
+    | otherwise = [ (-b + sqrt discr) / 2 / a
+                  , (-b - sqrt discr) / 2 / a ]
+  where discr = b^2 - 4*a*c
+
 -- | Convert a nested-functor matrix to an hmatrix Matrix
 toHMatrix :: (Functor m, Foldable m, Foldable n, LA.Element a)
           => m (n a) -> LA.Matrix a
